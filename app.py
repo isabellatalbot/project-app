4774465c589531b5a41af020b6cb1dd22913f72e
isabellatalbot <ijtalbot1@sheffield.ac.uk>
2017-11-29 19:08:30 +0000

    added stuff to make it work

--- app.py
+++ app.py
@@ -1,5 +1,13 @@
 import os
 
+from flask import Flask, render_template
+
+app = Flask("BasicApp")
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
 if 'PORT' in os.environ:
 	app.run(host='0.0.0.0', port=int(os.environ[PORT]))
 else:
